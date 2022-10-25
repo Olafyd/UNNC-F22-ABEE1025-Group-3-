09b140b3647a2416ca9fad3f22951ea7663e5804
Olafyd <ssyyf4@nottingham.edu.com>
2022-10-25 12:34:33 +0800

    ...

--- cw_part2_main.py
+++
@@ -1,4 +0,0 @@
-# Date of the first creation: 2022/10/18
-# This file is for Energyplus parametric simulation
-from parametric_simulation import run_one_parameter_parametric
-from post_processor import plot_1D_results
--- post_processor.py
+++
@@ -1,9 +0,0 @@
-# Date of the first creation: 2022/10/18
-# This file is for Energyplus parametric simulation
-import pandas as pd
-import datetime as dt
-import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
-
-def plot_1D_results(output_paths, plot_column_name, 
-y_axis_title, plot_title):

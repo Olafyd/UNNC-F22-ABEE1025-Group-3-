13b73041a89ab789933f91a888a39443a0e5ba36
Olafyd <ssyyf4@nottingham.edu.com>
2022-10-30 17:48:15 +0800

    I add a test py file

---
+++ test.py
@@ -0,0 +1,10 @@
+from parametric_simulation import run_one_parameter_parametric
+import numpy
+eplus_run_path = './energyplus9.5/energyplus'
+idf_path = './1ZoneUncontrolled_win_1.idf'
+parameter_key = ['WindowMaterial:SimpleGlazingSystem', 
+'SimpleWindow:DOUBLE PANE WINDOW', 'solar_heat_gain_coefficient'] 
+parameter_vals = numpy.arange(0.25,0.75,0.02)
+output_dir = './param_exp_1'
+run_one_parameter_parametric(eplus_run_path, idf_path, output_dir,
+                                parameter_key, parameter_vals)

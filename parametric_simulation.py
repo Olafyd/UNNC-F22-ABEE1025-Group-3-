39375c9024d62433cc093368a6b1d6f6f6f5cde4
Olafyd <ssyyf4@nottingham.edu.com>
2023-01-29 11:43:22 +0800

    ww

--- parametric_simulation.py
+++ parametric_simulation.py
@@ -52,60 +52,22 @@ def run_one_parameter_parametric(eplus_run_path, idf_path, output_dir,
     solar_heat_gain_coefficient values, ranging from 0.25 to 0.75 
     with 0.02 interval, for the model
     '''
-    output_paths = {}
+    res_dict = {}
 
     if not os.path.isdir(output_dir):
-        os.mkdir(output_dir)    
-    for i in range(len(parameter_vals)):
-        this_output_dir = output_dir + '/run_res' + str(i+1)
+        os.mkdir(output_dir) 
 
-        parameter_val = parameter_vals[i]
+    for parameter_val in parameter_vals:
+        this_output_dir = output_dir + f'/{parameter_val}'
 
         this_res_path = run_one_simulation_helper(eplus_run_path, idf_path, 
             this_output_dir, parameter_key, parameter_val)
 
-        output_paths[str(parameter_val)] = this_res_path
-
-    print (output_paths)
-    return output_paths
+        res_dict[parameter_val] = this_res_path
 
 
-    """
-    Args:
-    ----------
-    eplus_run_path: string type, the path to EnergyPlus executable
-    idf_path: string type, the path to EnergyPlus IDF file
-    output_dir: string type, the directory to store all simulation results.
-                Note: the simulation results from different simulations 
-                        must not overwrite each other. 
-    parameter_key: list type, each item in the list represents the key
-                 at different levels.
-                 For example, ['WindowMaterial:SimpleGlazingSystem', 
-                                'SimpleWindow:DOUBLE PANE WINDOW', 
-                                'solar_heat_gain_coefficient'] 
-                 means (assume json_model is the EnergyPlus JSON model) 
-                 json_model['WindowMaterial:SimpleGlazingSystem']\
-                            ['SimpleWindow:DOUBLE PANE WINDOW']\
-                            ['solar_heat_gain_coefficient'] 
-                 is the innermost key to be accessed.
-    parameter_vals: list type, each item in the list is corresponding to
-                    the one simulation in the parametric simulation. 
-                    For example, [0.1, 0.2, 0.3, 0.4, 0.5] 
-                    means five simulations will be run, 
-                    and the first simulation will change the value 
-                    of the key in parameter_key to 0.1, 
-                    the second will be 0.2, etc. 
-    Output:
-    ---------- 
-    output_paths: dict type, the key in the dict is the parameter 
-                    values corresponding to parameter_vals, 
-                    and the value is the path to the corresponding 
-                    eplusout.csv file. 
-                For example, {0.1: ‘param_sim_res/run_1/eplusout.csv’,
-                0.2: ‘param_sim_res/run_2/eplusout.csv’,
-                0.3: ‘param_sim_res/run_3/eplusout.csv’,
-                0.4: ‘param_sim_res/run_4/eplusout.csv’,
-                0.5: ‘param_sim_res/run_5/eplusout.csv’}
-    """
-    
-    return None # TO-DO!
+    return res_dict
+
+
+  
+
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ eplus_run_path = './energyplus9.5/energyplus'
 idf_path = './1ZoneUncontrolled_win_1.idf'
 parameter_key = ['WindowMaterial:SimpleGlazingSystem', 
 'SimpleWindow:DOUBLE PANE WINDOW', 'solar_heat_gain_coefficient'] 
-parameter_vals = numpy.arange(0.25,0.75,0.02)
+parameter_vals = [0.25 + i*0.02 for i in range(25)]
 output_dir = './param_exp_1'
 
 run_one_parameter_parametric(eplus_run_path, idf_path, output_dir,
